2b98bd1e68989496797e89b7a13642cf74b19240
Roman Sandler <romansandler@Romans-MacBook-Pro.local>
2020-03-08 13:58:47 +0200

    extract button component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React from "react"
 import { PricingCard } from "./components"
 
+const features = [
+  "Est superbus guttus, cesaris.",
+  "Sunt compateres amor dexter, magnum historiaes.",
+  "Cum finis persuadere, omnes urbses visum lotus, castus musaes.",
+  "Cum imber ire, omnes guttuses visum primus, dexter classises.",
+  "Altus nutrix unus transferres fluctus est."
+]
+
 function App() {
   return (
-    <div className="h-screen w-screen bg-gray-100 flex items-center justify-center">
-      <div className="container flex justify-center">
-        <PricingCard />
-        <PricingCard />
-        <PricingCard />
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="container justify-center items-center p-6 flex flex-col lg:flex-row">
+        <div className="w-full order-2 mt-6 lg:order-1 lg:w-1/4 lg:-mr-2 lg:mt-0">
+          <PricingCard
+            title="Hobby"
+            price={79}
+            features={features.slice(0, 3)}
+          />
+        </div>
+        <div className="w-full order-1 lg:order-2 lg:w-1/3">
+          <PricingCard title="Growth" price={149} popular features={features} />
+        </div>
+        <div className="mt-6 w-full order-3 lg:w-1/4 lg:-ml-2">
+          <PricingCard
+            title="Scale"
+            price={349}
+            features={features.slice(0, 3)}
+          />
+        </div>
       </div>
     </div>
   )
--- src/components/PricingCard.js
+++ src/components/PricingCard.js
@@ -1,43 +1,65 @@
 import React from "react"
+import cx from "classnames"
 import Check from "./Check"
+import PropTypes from "prop-types"
 
-function PricingCard(props) {
+PricingCard.propTypes = {
+  features: PropTypes.array.isRequired,
+  popular: PropTypes.bool,
+  price: PropTypes.number,
+  title: PropTypes.string.isRequired
+}
+
+function PricingCard({
+  popular = false,
+  features = [],
+  title = "",
+  price = 0
+}) {
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-lg w-1/3">
+    <div
+      className={cx(
+        "flex flex-col bg-white rounded-lg shadow-lg w-full relative",
+        {
+          "border-2 border-indigo-600 z-10": popular
+        }
+      )}
+    >
+      {popular && (
+        <div className="flex justify-center -mt-3">
+          <span className="bg-indigo-600 rounded-full text-indigo-100 px-4 uppercase text-xs h-6 leading-6">
+            Most Popular
+          </span>
+        </div>
+      )}
       <div className="p-8 flex flex-col items-center justify-center">
-        <h3 className="text-gray-900 font-semibold text-2xl">Growth</h3>
+        <h3 className="text-gray-900 font-semibold text-2xl">{title}</h3>
         <p className="flex mt-2">
           <span className="self-start text-gray-900 text-2xl">$</span>
           <span className="font-bold text-6xl text-gray-900 leading-none ml-1">
-            149
+            {price}
           </span>
           <span className="self-center text-gray-500 text-xl ml-2">/month</span>
         </p>
       </div>
-      <div className="p-8 bg-gray-100 border-gray-200 border-t">
+      <div className="p-8 bg-gray-100 border-gray-200 border-t rounded-b-lg">
         <ul>
-          <li className="text-gray-600 mt-3 text-sm flex items-center">
-            <Check />
-            <span className="ml-2">Cum cobaltum manducare, decores.</span>
-          </li>
-          <li className="text-gray-600 mt-3 text-sm flex items-center">
-            <Check />
-            <span className="ml-2">Superbus, fortis finiss tandem.</span>
-          </li>
-          <li className="text-gray-600 mt-3 text-sm flex items-center">
-            <Check />
-            <span className="ml-2">Cum xiphias studere, omnes.</span>
-          </li>
-          <li className="text-gray-600 mt-3 text-sm flex items-center">
-            <Check />
-            <span className="ml-2">Cum bubo resistere, omnes.</span>
-          </li>
-          <li className="text-gray-600 mt-3 text-sm flex items-center">
-            <Check />
-            <span className="ml-2">Fatalis, gratis fluctuss.</span>
-          </li>
+          {features.map(feature => (
+            <li
+              className="text-gray-600 mt-3 text-sm flex items-center"
+              key={feature}
+            >
+              <Check />
+              <span className="ml-2 truncate">{feature}</span>
+            </li>
+          ))}
         </ul>
-        <button className="mt-8 bg-indigo-600 text-indigo-100 text-lg text-center w-full p-2 rounded">
+        <button
+          className={cx("btn", {
+            "btn-primary": popular,
+            "btn-default": !popular
+          })}
+        >
           Start your trial
         </button>
       </div>
