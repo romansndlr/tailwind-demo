f6bc06d853a4b257aa4bd43f36b2733c7b1b8b8a
Roman Sandler <romansandler@Romans-MacBook-Pro.local>
2020-03-09 15:01:52 +0200

    add prettier and html boilerplate

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React from "react"
 
 function App() {
-  return <div>Hello world!</div>;
+  return (
+    <div>
+      <div>
+        <h3>Growth</h3>
+        <p>
+          <span>$</span>
+          <span>149</span>
+          <span>/month</span>
+        </p>
+      </div>
+      <div>
+        <ul>
+          <li>Sunt capioes pugna mirabilis, festus lixaes.</li>
+          <li>Sunt capioes pugna mirabilis, festus lixaes.</li>
+          <li>Sunt capioes pugna mirabilis, festus lixaes.</li>
+          <li>Sunt capioes pugna mirabilis, festus lixaes.</li>
+          <li>Sunt capioes pugna mirabilis, festus lixaes.</li>
+        </ul>
+        <button>Start your trial</button>
+      </div>
+    </div>
+  )
 }
 
-export default App;
+export default App
